15b60c33b07a7df4c61720378f07f46a0e448e72
tuanthanhngo <tuanthanhngo@yahoo.com>
2013-08-26 18:38:01 +0700

    update

--- tests/src/com/ttngo/guitargam/database/GGSQLiteHelperTest.java
+++ tests/src/com/ttngo/guitargam/database/GGSQLiteHelperTest.java
@@ -3,5 +3,8 @@ package com.ttngo.guitargam.database;
 import android.test.AndroidTestCase;
 
 public class GGSQLiteHelperTest extends AndroidTestCase {
+	/*
+	 * http://stackoverflow.com/questions/8499554/android-junit-test-for-sqliteopenhelper
+	 */
 	
 }
